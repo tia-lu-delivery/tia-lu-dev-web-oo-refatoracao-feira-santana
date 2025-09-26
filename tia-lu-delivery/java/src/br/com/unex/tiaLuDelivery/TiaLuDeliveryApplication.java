954c1c9c5edf4c26ee6336096ede64c7f04fc6f0
lasilva <lucas.almeida.las@gmail.com>
2025-09-26 16:49:56 -0300

    feature: adicionada as classes de modelo e dois pacotes: cli e repository

---
+++ tia-lu-delivery/java/src/br/com/unex/tiaLuDelivery/TiaLuDeliveryApplication.java
@@ -0,0 +1,9 @@
+package br.com.unex.tiaLuDelivery;
+
+import br.com.unex.tiaLuDelivery.cli.TiaLuCLI;
+
+public class TiaLuDeliveryApplication {
+    public static void main(String[] args) {
+        new TiaLuCLI().start();
+    }
+}
